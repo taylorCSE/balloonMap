ca4e8cf365f2c5cd00c2c7288d4897c22b380fb5
Jon Craton <jncraton@gmail.com>
2013-06-18 17:02:14 -0400

    removed extra includes

--- MapWindow/Form1.cs
+++ MapWindow/Form1.cs
@@ -1,15 +1,7 @@
 ﻿using System;
-using System.IO;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
 using MapPoint;
 using MySql.Data.MySqlClient;
-using System.Globalization;
 
 namespace MapWindow
 {
